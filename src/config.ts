12c2927e6fe1205c28f191fa0531f057a9232669
Nacho <nachofernandezmillera@gmail.com>
2022-09-26 22:14:10 +0200

    Add a default layout for pages

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'El SEO Blog de Ignacio 🚀';
+export const SITE_DESCRIPTION = 'Bienvenido a mi humilde blog personal';
+export const SITE_LANGUAGE = 'es';
