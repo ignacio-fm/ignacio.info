0234e2ed805a8efd31613a352302c107620ccc4d
Nacho <nachofernandezmillera@gmail.com>
2022-09-28 19:33:54 +0200

    Add Tailwind and @astrojs/image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ignacio.info',
-	experimental: {
-		integrations: true,
-	},
-	integrations: [mdx(), sitemap()],
+  site: 'https://ignacio.info',
+  experimental: {
+    integrations: true
+  },
+  integrations: [mdx(), sitemap(), tailwind(), image()]
 });
