fb827fd97b8511416d1d50e393b93ed983ccd823
Nacho <nachofernandezmillera@gmail.com>
2023-01-07 01:47:02 +0100

    Remove @astrojs/vercel in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
 import remarkGfm from "remark-gfm";
 import rehypePrism from "@mapbox/rehype-prism";
@@ -10,8 +9,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: "https://ignacio.info",
-  output: "server",
-  adapter: vercel(),
   markdown: {
     syntaxHighlight: "prism",
   },
