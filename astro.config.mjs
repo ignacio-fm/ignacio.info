bea1dbd33d2c8180b99034ad86de62350f33dd94
Nacho <nachofernandezmillera@gmail.com>
2023-01-07 22:11:28 +0100

    Install @astrojs/vercel/serverless again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,22 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
-// import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import remarkGfm from "remark-gfm";
-
-// https://astro.build/config
 import rehypePrism from "@mapbox/rehype-prism";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import sitemap from '@astrojs/sitemap';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ignacio.info",
-  // output: "server",
-  // adapter: vercel(),
   markdown: {
-    syntaxHighlight: "prism",
+    syntaxHighlight: "prism"
   },
-  integrations: [
-    react(),
-    image(),
-    mdx({
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [rehypePrism],
-    }),
-    sitemap(),
-  ],
+  integrations: [react(), image(), mdx({
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism]
+  }), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
