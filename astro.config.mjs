f44460254404321aeed45450c787478e72dc8c67
Nacho <nachofernandezmillera@gmail.com>
2023-01-07 01:55:14 +0100

    Edit @astrojs/vercel in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,31 @@
 import { defineConfig } from "astro/config";
+
+// https://astro.build/config
 import react from "@astrojs/react";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
 import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 import remarkGfm from "remark-gfm";
+
+// https://astro.build/config
 import rehypePrism from "@mapbox/rehype-prism";
+
+// https://astro.build/config
 import image from "@astrojs/image";
+
+// https://astro.build/config
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ignacio.info",
+  output: "server",
+  adapter: vercel(),
   markdown: {
     syntaxHighlight: "prism",
   },
